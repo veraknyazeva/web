90ccc53c577c27785bbfb3bed04f14b98f1e65b0
Vera Knyazeva <verashibanova@yandex.ru>
2024-08-14 18:24:11 +0500

    task 2

---
+++ src/main/java/Main.java
@@ -0,0 +1,58 @@
+import handler.RequestHandler;
+import model.Request;
+
+import java.io.BufferedOutputStream;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.util.logging.Handler;
+
+
+public class Main {
+    public static void main(String[] args) {
+        final var server = new Server();
+        // код инициализации сервера (из вашего предыдущего ДЗ)
+
+        //добавление хендлеров (обработчиков)
+        server.addHandler("GET", "/messages", new RequestHandler() {
+            public void handle(Request request, BufferedOutputStream out) {
+                try {
+                    final String response = "Hello word!";
+                    byte[] bytes = response.getBytes();
+                    final int length = bytes.length;
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + "text/plain" + "\r\n" +
+                                    "Content-Length: " + length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.write(bytes);
+                    out.flush();
+                } catch (Exception ex) {
+
+                }
+            }
+        });
+        server.addHandler("GET", "/what/time", new RequestHandler() {
+            public void handle(Request request, BufferedOutputStream out) {
+                try {
+                    final LocalDateTime response = LocalDateTime.now();
+                    byte[] bytes = response.toString().getBytes();
+                    final int length = bytes.length;
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + "text/plain" + "\r\n" +
+                                    "Content-Length: " + length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.write(bytes);
+                    out.flush();
+                } catch (Exception ex) {
+                }
+            }
+        });
+
+        server.start(9999);
+    }
+}
---
+++ src/main/java/model/Request.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.Map;
+
+public class Request {
+    private String httpMethod;
+    private String path;
+    private String protocol;
+    private Map<String, String> headers;
+    private String body;
+
+    public Request() {
+    }
+
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
+    public void setHttpMethod(String httpMethod) {
+        this.httpMethod = httpMethod;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
